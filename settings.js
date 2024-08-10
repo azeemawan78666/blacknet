90b92e9eab01cea44f78a22ddb5c3b5d5967a244
azeemawan78666 <elite.bunch.climb@myclkd.id>
2024-08-10 18:29:27 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// BOT-INFO
+global.botName = "ZHNZK"
+global.ownerName = "𐏓꯭. ᬊ͜͡𝑹Σ𝑯𝑴𝑨𝑵ꫂ⃟🇵🇰 ³"
+global.botNumber = "254771170558"
+global.devNumber = ["254771170558"]
+
+// ADMIN-PANEL
+global.locID = "1" // Jangan Diganti
+global.eggID = "15" // Jangan Diganti
+global.domain = "https://example.com" // Ganti Domain Lu
+global.key_plta = "-" // Isi Apikey Plta Lu
+global.key_pltc = "-" // Isi Apikey Pltc Lu
+global.nama_host = "𐏓꯭. ᬊ͜͡𝑹Σ𝑯𝑴𝑨𝑵ꫂ⃟🇵🇰 ³" // Ganti Nama Store Atau nama Host Lu
+
+// BOT-SETTING
+global.Auto_Typing = false // auto typing
+global.Auto_Recording = false // auto recording
+global.Auto_ReadPesan = false // auto read messages
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
